cade32da3fa93d9cc22c04064fd7c8a2692ac7af
Nikita <nikita.shalashkov@gmail.com>
2023-04-09 03:35:11 +0300

    html/css/js

---
+++ templates/js/script.js
@@ -0,0 +1,32 @@
+let taskList = [];
+
+function addTask() {
+	let newTask = document.getElementById("newTask").value;
+	if (newTask !== "") {
+		taskList.push(newTask);
+		document.getElementById("newTask").value = "";
+		displayTasks();
+	}
+}
+
+function removeTask(index) {
+	taskList.splice(index, 1);
+	displayTasks();
+}
+
+function displayTasks() {
+	let taskListElement = document.getElementById("taskList");
+	taskListElement.innerHTML = "";
+	for (let i = 0; i < taskList.length; i++) {
+		let task = taskList[i];
+		let li = document.createElement("li");
+		let span = document.createElement("span");
+		let button = document.createElement("button");
+		span.innerHTML = task;
+		button.innerHTML = "Delete";
+		button.onclick = function() { removeTask(i); };
+		li.appendChild(span);
+		li.appendChild(button);
+		taskListElement.appendChild(li);
+	}
+}
